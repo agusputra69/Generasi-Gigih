411eb1eb0dc0abc22c28300e7523e7b3e031fe1d
AgusPutra69 <putraagus258@gmail.com>
2021-07-09 16:59:13 +0800

    update HW2

--- Homework/Day 2/script.js
+++ Homework/Day 2/script.js
@@ -1,3 +1,21 @@
 function runPopup() {
   window.alert('Submitted');
 }
+// add API
+const url = 'https://gist.githubusercontent.com/aryapradipta9/e6492383477803b233916e01f36d5465/raw/66942c739d66d3774303f84071696aa865a07077/single-sample.json';
+fetch(url)
+  .then((res) => res.json())
+  .then((data) => console.log(data));
+
+const fetchBtn = document.getElementById('fetch-btn');
+const tracksContainer = document.getElementById('playlist-wrap');
+
+fetchBtn.addEventListener('click', () => {
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      renderNewCard(data);
+      console.log(data);
+    })
+    .catch((err) => alert(err));
+});
